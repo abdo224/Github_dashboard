f9745b9f6f59bac90cc349bfaa32542d20ae5ec3
abdo224 <ibenaziza@inttic.dz>
2022-08-02 11:29:03 +0100

    new commit

--- mysite/Dockerfile
+++ mysite/Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.8-slim-buster
+FROM python:3.8
 ENV PYTHONUNBUFFERED=1
 WORKDIR /django
 COPY requirements.txt requirements.txt
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
+# DATABASES = {
+# 'default': {
+# 'ENGINE': 'django.db.backends.sqlite3',
+# 'NAME': BASE_DIR / 'db.sqlite3',
+# }
+# }
+DATABASE = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backend.mysql',
+        'NAME': 'django-app-db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'db',
+        'PORT': '3306',
+
     }
 }
 
