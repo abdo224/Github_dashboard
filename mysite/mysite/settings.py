cde202800db8a67cc303d32dde0999a8c875d8e5
abdo224 <ibenaziza@inttic.dz>
2022-08-14 10:18:22 +0100

    new commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 # 'default': {
-#    'ENGINE': 'django.db.backends.mysql',
-#   'NAME': 'django-app-db',
-#  'USER': 'root',
-# 'PASSWORD': '',
-# 'HOST': 'db',
-# 'PORT': '3306',
+# 'ENGINE': 'django.db.backends.sqlite3',
+# 'NAME': BASE_DIR / 'db.sqlite3',
+# }
+# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-app-db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'db',
+        'PORT': '3306',
 
-# }
-# }
+    }
+}
 
 
 # Password validation
