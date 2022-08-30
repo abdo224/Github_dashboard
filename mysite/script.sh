4c75efadd0f18534e0e3f2f5ccca9c6ed4e8f5c1
abdo224 <ibenaziza@inttic.dz>
2022-08-30 14:26:17 +0100

    new commit

--- mysite/script.sh
+++ mysite/script.sh
@@ -2,7 +2,8 @@ sudo systemctl start docker
 docker-compose down 
 docker rmi $(docker images)
 docker system prune -y
-docker-compose pull
+docker pull mysql
+docker pull djawed22/repo:latest
 docker-compose start db 
-slepp 30
+sleep 30
 docker-compose up -d 
